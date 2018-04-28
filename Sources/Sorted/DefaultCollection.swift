10f17e5fa41edb84b3e3ffbc8c830d58427bbf2d
Kare Morstol <kare@nottoobadsoftware.com>
2018-04-28 20:09:11 +0200

    .

--- Sources/Sorted/DefaultCollection.swift
+++ Sources/Sorted/DefaultCollection.swift
@@ -12,29 +12,66 @@ protocol DefaultCollection: Collection where Index == MyElements.Index, Element 
 }
 
 extension DefaultCollection {
-	var startIndex: Index { return elements.startIndex }
-	var endIndex: Index { return elements.endIndex }
+	var startIndex: MyElements.Index { return elements.startIndex }
+	var endIndex: MyElements.Index { return elements.endIndex }
 	subscript(position: MyElements.Index) -> Element { return elements[position] }
 	func index(after i: Index) -> Index { return elements.index(after: i) }
 	func makeIterator() -> MyElements.Iterator { return elements.makeIterator() }
 }
 
 
-struct SortedArray<Element>: DefaultCollection {
+struct SortedArray<Element: Comparable>: DefaultCollection, SortedCollection {
 	var elements: [Element]
+
+	public typealias Comparator<A> = (A, A) -> Bool
+	let areInIncreasingOrder: (Element, Element) -> Bool
 }
 
+extension SortedArray {
+	/// Initializes an empty array.
+	///
+	/// - Parameter areInIncreasingOrder: The comparison predicate the array should use to sort its elements.
+	public init(areInIncreasingOrder: @escaping Comparator<Element>) {
+		self.elements = []
+		self.areInIncreasingOrder = areInIncreasingOrder
+	}
 
+	/// Initializes the array with a sequence of unsorted elements and a comparison predicate.
+	public init<S: Sequence>(unsorted: S, areInIncreasingOrder: @escaping Comparator<Element>) where S.Iterator.Element == Element {
+		let sorted = unsorted.sorted(by: areInIncreasingOrder)
+		self.elements = sorted
+		self.areInIncreasingOrder = areInIncreasingOrder
+	}
 
-struct SA<Element>: Collection {
-	typealias MyElements = [Element]
-	typealias Index = MyElements.Index
+	/// Initializes the array with a sequence that is already sorted according to the given comparison predicate.
+	///
+	/// This is faster than `init(unsorted:areInIncreasingOrder:)` because the elements don't have to sorted again.
+	///
+	/// - Precondition: `sorted` is sorted according to the given comparison predicate. If you violate this condition, the behavior is undefined.
+	public init<S: Sequence>(sorted: S, areInIncreasingOrder: @escaping Comparator<Element>) where S.Iterator.Element == Element {
+		self.elements = Array(sorted)
+		self.areInIncreasingOrder = areInIncreasingOrder
+	}
+}
 
-	var elements: MyElements
+extension SortedArray where Element: Comparable {
+	/// Initializes an empty sorted array. Uses `<` as the comparison predicate.
+	init() {
+		self.init(areInIncreasingOrder: <)
+	}
 
-	var startIndex: MyElements.Index { return elements.startIndex }
-	var endIndex: MyElements.Index { return elements.endIndex }
-	subscript(position: Index) -> Element { return elements[position] }
-	func index(after i: Index) -> Index { return elements.index(after: i) }
+	/// Initializes the array with a sequence of unsorted elements. Uses `<` as the comparison predicate.
+	init<S: Sequence>(unsorted: S) where S.Iterator.Element == Element {
+		self.init(unsorted: unsorted, areInIncreasingOrder: <)
+	}
+
+	/// Initializes the array with a sequence that is already sorted according to the `<` comparison predicate. Uses `<` as the comparison predicate.
+	///
+	/// This is faster than `init(unsorted:)` because the elements don't have to sorted again.
+	///
+	/// - Precondition: `sorted` is sorted according to the `<` predicate. If you violate this condition, the behavior is undefined.
+	init<S: Sequence>(sorted: S) where S.Iterator.Element == Element {
+		self.init(sorted: sorted, areInIncreasingOrder: <)
+	}
 }
 
---
+++ Tests/SortedTests/SortedArrayTests.swift
@@ -0,0 +1,403 @@
+
+@testable import Sorted
+import XCTest
+
+class SortedArrayTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+    func testLinuxTestSuiteIncludesAllTests() {
+        #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
+            #if swift(>=4.0)
+                let darwinTestCount = SortedArrayTests.defaultTestSuite.testCaseCount
+            #else
+                let darwinTestCount = Int(SortedArrayTests.defaultTestSuite().testCaseCount)
+            #endif
+            let linuxTestCount = SortedArrayTests.allTests.count
+            XCTAssertEqual(linuxTestCount, darwinTestCount, "allTests (used for testing on Linux) is missing \(darwinTestCount - linuxTestCount) tests")
+        #endif
+    }
+
+    func testInitUnsortedSorts() {
+        let sut = SortedArray(unsorted: [3,4,2,1], areInIncreasingOrder: <)
+        assertElementsEqual(sut, [1,2,3,4])
+    }
+
+    func testInitSortedDoesntResort() {
+        // Warning: this is not a valid way to create a SortedArray
+        let sut = SortedArray(sorted: [3,2,1])
+        assertElementsEqual(Array(sut), [3,2,1])
+    }
+
+    func testSortedArrayCanUseArbitraryComparisonPredicate() {
+        struct Person {
+            var firstName: String
+            var lastName: String
+        }
+        let a = Person(firstName: "A", lastName: "Smith")
+        let b = Person(firstName: "B", lastName: "Jones")
+        let c = Person(firstName: "C", lastName: "Lewis")
+
+        var sut = SortedArray<Person> { $0.firstName > $1.firstName }
+        sut.insert(contentsOf: [b,a,c])
+        assertElementsEqual(sut.map { $0.firstName }, ["C","B","A"])
+    }
+
+    func testConvenienceInitsUseLessThan() {
+        let sut = SortedArray(unsorted: ["a","c","b"])
+        assertElementsEqual(sut, ["a","b","c"])
+    }
+
+    func testInsertAtBeginningPreservesSortOrder() {
+        var sut = SortedArray(unsorted: 1...3)
+        sut.insert(0)
+        assertElementsEqual(sut, [0,1,2,3])
+    }
+
+    func testInsertInMiddlePreservesSortOrder() {
+        var sut = SortedArray(unsorted: 1...5)
+        sut.insert(4)
+        assertElementsEqual(sut, [1,2,3,4,4,5])
+    }
+
+    func testInsertAtEndPreservesSortOrder() {
+        var sut = SortedArray(unsorted: 1...3)
+        sut.insert(5)
+        assertElementsEqual(sut, [1,2,3,5])
+    }
+
+    func testInsertAtBeginningReturnsInsertionIndex() {
+        var sut = SortedArray(unsorted: [1,2,3])
+        let index = sut.insert(0)
+        XCTAssertEqual(index, 0)
+    }
+
+    func testInsertInMiddleReturnsInsertionIndex() {
+        var sut = SortedArray(unsorted: [1,2,3,5])
+        let index = sut.insert(4)
+        XCTAssertEqual(index, 3)
+    }
+
+    func testInsertAtEndReturnsInsertionIndex() {
+        var sut = SortedArray(unsorted: [1,2,3])
+        let index = sut.insert(100)
+        XCTAssertEqual(index, 3)
+    }
+
+    func testInsertInEmptyArrayReturnsInsertionIndex() {
+        var sut = SortedArray<Int>()
+        let index = sut.insert(10)
+        XCTAssertEqual(index, 0)
+    }
+
+    func testInsertEqualElementReturnsCorrectInsertionIndex() {
+        var sut = SortedArray(unsorted: [3,1,0,2,1])
+        let index = sut.insert(1)
+        XCTAssert(index == 1 || index == 2 || index == 3)
+    }
+
+    func testInsertContentsOfPreservesSortOrder() {
+        var sut = SortedArray(unsorted: [10,9,8])
+        sut.insert(contentsOf: (7...11).reversed())
+        assertElementsEqual(sut, [7,8,8,9,9,10,10,11])
+    }
+
+    func testIndexOfFindsElementInMiddle() {
+        let sut = SortedArray(unsorted: ["a","z","r","k"])
+        let index = sut.index(of: "k")
+        XCTAssertEqual(index, 1)
+    }
+
+    func testIndexOfFindsFirstElement() {
+        let sut = SortedArray(sorted: 1..<10)
+        let index = sut.index(of: 1)
+        XCTAssertEqual(index, 0)
+    }
+
+    func testIndexOfFindsLastElement() {
+        let sut = SortedArray(sorted: 1..<10)
+        let index = sut.index(of: 9)
+        XCTAssertEqual(index, 8)
+    }
+
+    func testIndexOfReturnsNilWhenNotFound() {
+        let sut = SortedArray(unsorted: "Hello World".characters)
+        let index = sut.index(of: "h")
+        XCTAssertNil(index)
+    }
+
+    func testIndexOfReturnsNilForEmptyArray() {
+        let sut = SortedArray<Int>()
+        let index = sut.index(of: 1)
+        XCTAssertNil(index)
+    }
+
+    func testIndexOfCanDealWithSingleElementArray() {
+        let sut = SortedArray<Int>(unsorted: [5])
+        let index = sut.index(of: 5)
+        XCTAssertEqual(index, 0)
+    }
+
+    func testIndexOfFindsFirstIndexOfDuplicateElements1() {
+        let sut = SortedArray(unsorted: [1,2,3,3,3,3,3,3,3,3,4,5])
+        let index = sut.index(of: 3)
+        XCTAssertEqual(index, 2)
+    }
+
+    func testIndexOfFindsFirstIndexOfDuplicateElements2() {
+        let sut = SortedArray(unsorted: [1,4,4,4,4,4,4,4,4,3,2])
+        let index = sut.index(of: 4)
+        XCTAssertEqual(index, 3)
+    }
+
+    func testIndexOfFindsFirstIndexOfDuplicateElements3() {
+        let sut = SortedArray(unsorted: String(repeating: "A", count: 10).characters)
+        let index = sut.index(of: "A")
+        XCTAssertEqual(index, 0)
+    }
+
+    func testIndexOfFindsFirstIndexOfDuplicateElements4() {
+        let sut = SortedArray<Character>(unsorted: Array(repeating: "a", count: 100_000))
+        let index = sut.index(of: "a")
+        XCTAssertEqual(index, 0)
+    }
+
+    func testIndexOfFindsFirstIndexOfDuplicateElements5() {
+        let sourceArray = Array(repeating: 5, count: 100_000) + [1,2,6,7,8,9]
+        let sut = SortedArray(unsorted: sourceArray)
+        let index = sut.index(of: 5)
+        XCTAssertEqual(index, 2)
+    }
+
+    func testLastIndexOfFindsElementInMiddle() {
+        let sut = SortedArray(unsorted: ["a","z","r","k"])
+        let index = sut.lastIndex(of: "k")
+        XCTAssertEqual(index, 1)
+    }
+
+    func testLastIndexOfFindsFirstElement() {
+        let sut = SortedArray(sorted: 1..<10)
+        let index = sut.lastIndex(of: 1)
+        XCTAssertEqual(index, 0)
+    }
+
+    func testLastIndexOfFindsLastElement() {
+        let sut = SortedArray(sorted: 1..<10)
+        let index = sut.lastIndex(of: 9)
+        XCTAssertEqual(index, 8)
+    }
+
+    func testLastIndexOfReturnsNilWhenNotFound() {
+        let sut = SortedArray(unsorted: "Hello World".characters)
+        let index = sut.lastIndex(of: "h")
+        XCTAssertNil(index)
+    }
+
+    func testLastIndexOfReturnsNilForEmptyArray() {
+        let sut = SortedArray<Int>()
+        let index = sut.lastIndex(of: 1)
+        XCTAssertNil(index)
+    }
+
+    func testLastIndexOfCanDealWithSingleElementArray() {
+        let sut = SortedArray<Int>(unsorted: [5])
+        let index = sut.lastIndex(of: 5)
+        XCTAssertEqual(index, 0)
+    }
+
+    func testLastIndexOfFindsLastIndexOfDuplicateElements1() {
+        let sut = SortedArray(unsorted: [1,2,3,3,3,3,3,3,3,3,4,5])
+        let index = sut.lastIndex(of: 3)
+        XCTAssertEqual(index, 9)
+    }
+
+    func testLastIndexOfFindsLastIndexOfDuplicateElements2() {
+        let sut = SortedArray(unsorted: [1,4,4,4,4,4,4,4,4,3,2])
+        let index = sut.lastIndex(of: 4)
+        XCTAssertEqual(index, 10)
+    }
+
+    func testLastIndexOfFindsLastIndexOfDuplicateElements3() {
+        let sut = SortedArray(unsorted: String(repeating: "A", count: 10).characters)
+        let index = sut.lastIndex(of: "A")
+        XCTAssertEqual(index, 9)
+    }
+
+    func testsContains() {
+        let sut = SortedArray(unsorted: "Lorem ipsum".characters)
+        XCTAssertTrue(sut.contains(" "))
+        XCTAssertFalse(sut.contains("a"))
+    }
+
+    func testMin() {
+        let sut = SortedArray(unsorted: -10...10)
+        XCTAssertEqual(sut.min(), -10)
+    }
+
+    func testMax() {
+        let sut = SortedArray(unsorted: -10...(-1))
+        XCTAssertEqual(sut.max(), -1)
+    }
+
+    func testCustomStringConvertible() {
+        let sut = SortedArray(unsorted: ["a", "c", "b"])
+        let description = String(describing: sut)
+        XCTAssertEqual(description, "[\"a\", \"b\", \"c\"] (sorted)")
+    }
+
+    func testCustomDebugStringConvertible() {
+        let sut = SortedArray(unsorted: ["a", "c", "b"])
+        let description = String(reflecting: sut)
+        XCTAssertEqual(description, "<SortedArray> [\"a\", \"b\", \"c\"]")
+    }
+
+    func testFilter() {
+        let sut = SortedArray(unsorted: ["a", "b", "c"])
+        assertElementsEqual(sut.filter { $0 != "a" }, ["b", "c"])
+    }
+
+    func testRemoveAtIndex() {
+        var sut = SortedArray(unsorted: [3,4,2,1])
+        let removedElement = sut.remove(at: 1)
+        assertElementsEqual(sut, [1,3,4])
+        XCTAssertEqual(removedElement, 2)
+    }
+
+    func testRemoveSubrange() {
+        var sut = SortedArray(unsorted: ["a","d","c","b"])
+        sut.removeSubrange(2..<4)
+        assertElementsEqual(sut, ["a","b"])
+    }
+
+    func testRemoveFirst() {
+        var sut = SortedArray(unsorted: [3,4,2,1])
+        let removedElement = sut.removeFirst()
+        assertElementsEqual(sut, [2,3,4])
+        XCTAssertEqual(removedElement, 1)
+    }
+
+    func testRemoveFirstN() {
+        var sut = SortedArray(unsorted: [3,4,2,1])
+        sut.removeFirst(2)
+        assertElementsEqual(sut, [3,4])
+    }
+
+    func testRemoveLast() {
+        var sut = SortedArray(unsorted: [3,4,2,1])
+        let removedElement = sut.removeLast()
+        assertElementsEqual(sut, [1,2,3])
+        XCTAssertEqual(removedElement, 4)
+    }
+
+    func testRemoveLastN() {
+        var sut = SortedArray(unsorted: [3,4,2,1])
+        sut.removeLast(2)
+        assertElementsEqual(sut, [1,2])
+    }
+
+    func testRemoveAll() {
+        var sut = SortedArray(unsorted: ["a","d","c","b"])
+        sut.removeAll()
+        assertElementsEqual(sut, [])
+    }
+
+    func testRemoveElementAtBeginningPreservesSortOrder() {
+        var sut = SortedArray(unsorted: 1...3)
+        sut.remove(1)
+        assertElementsEqual(sut, [2,3])
+    }
+
+    func testRemoveElementInMiddlePreservesSortOrder() {
+        var sut = SortedArray(unsorted: 1...5)
+        sut.remove(4)
+        assertElementsEqual(sut, [1,2,3,5])
+    }
+
+    func testRemoveElementAtEndPreservesSortOrder() {
+        var sut = SortedArray(unsorted: 1...3)
+        sut.remove(3)
+        assertElementsEqual(sut, [1,2])
+    }
+
+    func testImplementsEqual() {
+        let sut = SortedArray(unsorted: [3,2,1])
+        XCTAssertTrue(sut == SortedArray(unsorted: 1...3))
+    }
+
+    func testImplementsNotEqual() {
+        let sut = SortedArray(unsorted: 1...3)
+        XCTAssertTrue(sut != SortedArray(unsorted: 1...4))
+    }
+}
+
+
+func assertElementsEqual<S1, S2>(_ expression1: @autoclosure () throws -> S1, _ expression2: @autoclosure () throws -> S2, _ message: @autoclosure () -> String = "", file: StaticString = #file, line: UInt = #line)
+	where S1: Sequence, S2: Sequence, S1.Iterator.Element == S2.Iterator.Element, S1.Iterator.Element: Equatable {
+
+		// This should give a better error message than using XCTAssert(try expression1().elementsEqual(expression2()), ...)
+		XCTAssertEqual(Array(try expression1()), Array(try expression2()), message, file: file, line: line)
+}
+
+extension SortedArrayTests {
+    static var allTests : [(String, (SortedArrayTests) -> () throws -> Void)] {
+        return [
+            ("testLinuxTestSuiteIncludesAllTests", testLinuxTestSuiteIncludesAllTests),
+            ("testInitUnsortedSorts", testInitUnsortedSorts),
+            ("testInitSortedDoesntResort", testInitSortedDoesntResort),
+            ("testSortedArrayCanUseArbitraryComparisonPredicate", testSortedArrayCanUseArbitraryComparisonPredicate),
+            ("testConvenienceInitsUseLessThan", testConvenienceInitsUseLessThan),
+            ("testInsertAtBeginningPreservesSortOrder", testInsertAtBeginningPreservesSortOrder),
+            ("testInsertInMiddlePreservesSortOrder", testInsertInMiddlePreservesSortOrder),
+            ("testInsertAtEndPreservesSortOrder", testInsertAtEndPreservesSortOrder),
+            ("testInsertAtBeginningReturnsInsertionIndex", testInsertAtBeginningReturnsInsertionIndex),
+            ("testInsertInMiddleReturnsInsertionIndex", testInsertInMiddleReturnsInsertionIndex),
+            ("testInsertAtEndReturnsInsertionIndex", testInsertAtEndReturnsInsertionIndex),
+            ("testInsertInEmptyArrayReturnsInsertionIndex", testInsertInEmptyArrayReturnsInsertionIndex),
+            ("testInsertEqualElementReturnsCorrectInsertionIndex", testInsertEqualElementReturnsCorrectInsertionIndex),
+            ("testInsertContentsOfPreservesSortOrder", testInsertContentsOfPreservesSortOrder),
+            ("testIndexOfFindsElementInMiddle", testIndexOfFindsElementInMiddle),
+            ("testIndexOfFindsFirstElement", testIndexOfFindsFirstElement),
+            ("testIndexOfFindsLastElement", testIndexOfFindsLastElement),
+            ("testIndexOfReturnsNilWhenNotFound", testIndexOfReturnsNilWhenNotFound),
+            ("testIndexOfReturnsNilForEmptyArray", testIndexOfReturnsNilForEmptyArray),
+            ("testIndexOfCanDealWithSingleElementArray", testIndexOfCanDealWithSingleElementArray),
+            ("testIndexOfFindsFirstIndexOfDuplicateElements1", testIndexOfFindsFirstIndexOfDuplicateElements1),
+            ("testIndexOfFindsFirstIndexOfDuplicateElements2", testIndexOfFindsFirstIndexOfDuplicateElements2),
+            ("testIndexOfFindsFirstIndexOfDuplicateElements3", testIndexOfFindsFirstIndexOfDuplicateElements3),
+            ("testIndexOfFindsFirstIndexOfDuplicateElements4", testIndexOfFindsFirstIndexOfDuplicateElements4),
+            ("testIndexOfFindsFirstIndexOfDuplicateElements5", testIndexOfFindsFirstIndexOfDuplicateElements4),
+            ("testLastIndexOfFindsElementInMiddle", testLastIndexOfFindsElementInMiddle),
+            ("testLastIndexOfFindsFirstElement", testLastIndexOfFindsFirstElement),
+            ("testLastIndexOfFindsLastElement", testLastIndexOfFindsLastElement),
+            ("testLastIndexOfReturnsNilWhenNotFound", testLastIndexOfReturnsNilWhenNotFound),
+            ("testLastIndexOfReturnsNilForEmptyArray", testLastIndexOfReturnsNilForEmptyArray),
+            ("testLastIndexOfCanDealWithSingleElementArray", testLastIndexOfCanDealWithSingleElementArray),
+            ("testLastIndexOfFindsLastIndexOfDuplicateElements1", testLastIndexOfFindsLastIndexOfDuplicateElements1),
+            ("testLastIndexOfFindsLastIndexOfDuplicateElements2", testLastIndexOfFindsLastIndexOfDuplicateElements2),
+            ("testLastIndexOfFindsLastIndexOfDuplicateElements3", testLastIndexOfFindsLastIndexOfDuplicateElements3),
+            ("testsContains", testsContains),
+            ("testMin", testMin),
+            ("testMax", testMax),
+            ("testCustomStringConvertible", testCustomStringConvertible),
+            ("testCustomDebugStringConvertible", testCustomDebugStringConvertible),
+            ("testFilter", testFilter),
+            ("testRemoveAtIndex", testRemoveAtIndex),
+            ("testRemoveSubrange", testRemoveSubrange),
+            ("testRemoveFirst", testRemoveFirst),
+            ("testRemoveFirstN", testRemoveFirstN),
+            ("testRemoveLast", testRemoveLast),
+            ("testRemoveLastN", testRemoveLastN),
+            ("testRemoveAll", testRemoveAll),
+            ("testRemoveElementAtBeginningPreservesSortOrder", testRemoveElementAtBeginningPreservesSortOrder),
+            ("testRemoveElementInMiddlePreservesSortOrder", testRemoveElementInMiddlePreservesSortOrder),
+            ("testRemoveElementAtEndPreservesSortOrder", testRemoveElementAtEndPreservesSortOrder),
+            ("testImplementsEqual", testImplementsEqual),
+            ("testImplementsNotEqual", testImplementsNotEqual),
+        ]
+    }
+}
