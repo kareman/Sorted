9261746ccffcff85c02fec7c1e3605209cc09779
Kare Morstol <kare@nottoobadsoftware.com>
2018-05-08 20:53:35 +0200

    Use toolchain DEVELOPMENT-SNAPSHOT-2018-04-25-a

--- Sorted.attabench/run.sh
+++ Sorted.attabench/run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 set -e
-exec xcrun swift run -c release -Xswiftc -whole-module-optimization Benchmark "$@"
+export TOOLCHAINS=org.swift.4220180425a
+swift --version
+swift run -c release -Xswiftc -whole-module-optimization Benchmark "$@"
