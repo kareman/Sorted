7a3e01435940d2c409cfe02973e537826722d202
Kare Morstol <kare@nottoobadsoftware.com>
2018-05-10 18:36:29 +0200

    Cleanup nonrandom access benchmarks.

--- Sorted.attabench/Sources/main.swift
+++ Sorted.attabench/Sources/main.swift
@@ -1,9 +1,5 @@
-// Copyright © 2017 Károly Lőrentey.
-// This file is part of Attabench: https://github.com/attaswift/Attabench
-// For licensing information, see the file LICENSE.md in the Git repository above.
 
 import Benchmarking
-//import Sorted
 
 let text = """
 	The ☀️ and 🌙 are in the sky,
@@ -13,9 +9,9 @@ let text = """
 let textCount = text.count
 
 
-let benchmark = Benchmark<(String)>(title: ".indices.contains") { size in
-	let l = size.quotientAndRemainder(dividingBy: textCount)
-	return String(repeating: text, count: l.quotient) + text.prefix(l.remainder)
+let benchmark = Benchmark<(String)>(title: "DefaultIndices<String>.firstIndex") { size in
+	let x = size.quotientAndRemainder(dividingBy: textCount)
+	return String(repeating: text, count: x.quotient) + text.prefix(x.remainder)
 }
 
 benchmark.descriptiveTitle = "Time spent on all elements"
@@ -31,37 +27,15 @@ benchmark.addTask(title: "DefaultIndices.firstIndex") { input in
 	}
 }
 
-extension DefaultIndices where Elements: BidirectionalCollection {
-	public func new_contains(_ element: Element) -> Bool {
-		guard element < endIndex else { return false }
-		for index in self {
-			if !(index < element) {
-				return index == element
-			}
-		}
-		return false
-	}
-
-	public func new_bad_contains(_ element: Element) -> Bool {
-		guard element < endIndex else { return false }
-		for index in self {
-			if index == element { return true }
-			if !(index < element) {	return false }
-		}
-		return false
-	}
-}
-/*
-benchmark.addTask(title: "newContains(valid index)") { input in
+benchmark.addTask(title: "forward_firstIndex") { input in
 	let indices = input.indices
 
 	return { timer in
 		for value in indices {
-			guard indices.new_contains(value) else { fatalError() }
+			guard indices.forward_firstIndex(of: value) == value else { fatalError() }
 		}
 	}
 }
-*/
 
 benchmark.addTask(title: "binarySearch_firstIndex") { input in
 	let indices = input.indices
@@ -73,22 +47,13 @@ benchmark.addTask(title: "binarySearch_firstIndex") { input in
 	}
 }
 
-benchmark.addTask(title: "binarySearchWithCount_firstIndex") { input in
+benchmark.addTask(title: "binarySearchCacheCount_firstIndex") { input in
 	let indices = input.indices
 
 	return { timer in
 		for value in indices {
-			guard indices.binarySearch2_firstIndex(of: value) == value else { fatalError("binarySearchWithCount_firstIndex") }
-		}
-	}
-}
-
-benchmark.addTask(title: "forward_firstIndex") { input in
-	let indices = input.indices
-
-	return { timer in
-		for value in indices {
-			guard indices.forward_firstIndex(of: value) == value else { fatalError() }
+			guard indices.binarySearchCacheCount_firstIndex(of: value) == value
+				else { fatalError("binarySearchCacheCount_firstIndex") }
 		}
 	}
 }
